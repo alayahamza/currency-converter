bbd307a248b98f4213ba2cdafdc11a0098d65aa6
hamzaalaya <alaya.hamza@gmail.com>
2017-03-13 17:58:15 +0100

    adding currency service

--- mobile/src/main/java/com/example/halaya/currencyconverter/MainActivity.java
+++ mobile/src/main/java/com/example/halaya/currencyconverter/MainActivity.java
@@ -10,7 +10,12 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 
+import com.example.halaya.currencyconverter.services.CurrencyService;
 import com.example.halaya.currencyconverter.utils.Currencies;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,17 +27,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinnerRight = (Spinner) findViewById(R.id.spinnerRight);
-        ArrayAdapter<CharSequence> adapterRight = ArrayAdapter.createFromResource(this,
+        String[] currencyArray = getResources().getStringArray(R.array.currencies);
+        String defualtCurrency = currencyArray[0];
+        ArrayAdapter<CharSequence> currencyAdapter = ArrayAdapter.createFromResource(this,
                 R.array.currencies, android.R.layout.simple_spinner_item);
-        adapterRight.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerRight.setAdapter(adapterRight);
+        fillCurrencyList(R.id.spinnerRight, currencyAdapter);
+        fillCurrencyList(R.id.spinnerLeft, currencyAdapter);
 
-        Spinner spinnerLeft = (Spinner) findViewById(R.id.spinnerLeft);
-        ArrayAdapter<CharSequence> adapterLeft = ArrayAdapter.createFromResource(this,
-                R.array.currencies, android.R.layout.simple_spinner_item);
-        adapterLeft.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerLeft.setAdapter(adapterLeft);
+        ListView exchangeRateComparedToDefaultCurrency=(ListView)findViewById(R.id.currentExchangeRates);
+
+
+
+
+        ArrayList<String> exchangeRates = callCurrencyService();
+        ArrayAdapter<String> exchangeRateArrayAdapter =
+                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, exchangeRates);
+        exchangeRateComparedToDefaultCurrency.setAdapter(exchangeRateArrayAdapter);
+    }
+
+    protected Spinner fillCurrencyList(int spinnerItemId, ArrayAdapter arrayAdapter) {
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinner = (Spinner) findViewById(spinnerItemId);
+        spinner.setAdapter(arrayAdapter);
+        return spinner;
+    }
+
+    protected ArrayList<String> callCurrencyService(){
+        ArrayList<String> exchangeRates = new ArrayList<String>();
+        CurrencyService currencyService = new CurrencyService();
+
+        try {
+            JSONObject json = new JSONObject(currencyService.requestContent("http://api.fixer.io/latest"));
+            JSONObject dataObject = json.getJSONObject("data");
+            JSONArray items = dataObject.getJSONArray("items");
+
+            for (int i = 0; i < items.length(); i++) {
+                JSONObject rateObject = items.getJSONObject(i);
+                exchangeRates.add(rateObject.getString("rates"));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return exchangeRates;
     }
 
 
