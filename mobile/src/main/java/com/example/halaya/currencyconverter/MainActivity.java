c4b9122f1e1acde16537e801b73939cbc7dd2749
hamzaalaya <alaya.hamza@gmail.com>
2017-03-15 12:12:56 +0100

    currency converted ;)

--- mobile/src/main/java/com/example/halaya/currencyconverter/MainActivity.java
+++ mobile/src/main/java/com/example/halaya/currencyconverter/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.halaya.currencyconverter;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 
@@ -14,6 +17,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -28,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayAdapter<String> exchangeRateArrayAdapter;
     String defaultCurrency;
+    EditText valueToConvert;
+    Response<LatestExchangeRates> LatestRatesResponse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,16 +41,18 @@ public class MainActivity extends AppCompatActivity {
 
         ListView exchangeRateComparedToDefaultCurrency = (ListView) findViewById(R.id.currentExchangeRates);
         String[] currencyArray = getResources().getStringArray(R.array.currencies);
-         defaultCurrency = currencyArray[0];
+        defaultCurrency = currencyArray[0];
         ArrayAdapter<CharSequence> currencyAdapter = ArrayAdapter.createFromResource(this,
                 R.array.currencies, android.R.layout.simple_spinner_item);
         fillCurrencyList(R.id.spinnerLeft, currencyAdapter);
         exchangeRateArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
         exchangeRateComparedToDefaultCurrency.setAdapter(exchangeRateArrayAdapter);
-        final Spinner currencySpinner = (Spinner)findViewById(R.id.spinnerLeft);
+        final Spinner currencySpinner = (Spinner) findViewById(R.id.spinnerLeft);
+        valueToConvert = (EditText) findViewById(R.id.valueToConvert);
+        valueToConvert.setText("1");
         currencySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                defaultCurrency =  currencySpinner.getSelectedItem().toString();
+                defaultCurrency = currencySpinner.getSelectedItem().toString();
                 getRetrofitObject();
             }
 
@@ -52,10 +60,23 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
         });
+
+        valueToConvert.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {}
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                updateDisplay(LatestRatesResponse);
+            }
+        });
     }
 
     void getRetrofitObject() {
-        ArrayList<String> rates = new ArrayList<>();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://api.fixer.io")
@@ -69,47 +90,9 @@ public class MainActivity extends AppCompatActivity {
         call.enqueue(new Callback<LatestExchangeRates>() {
             @Override
             public void onResponse(Response<LatestExchangeRates> response, Retrofit retrofit) {
-
+                LatestRatesResponse = response;
                 try {
-                    List<String> rates = new ArrayList<>();
-
-                    rates.add("EUR : " + response.body().getRates().getEUR());
-                    rates.add("AUD : " + response.body().getRates().getAUD());
-                    rates.add("BGN : " + response.body().getRates().getBGN());
-                    rates.add("BRL : " + response.body().getRates().getBRL());
-                    rates.add("CAD : " + response.body().getRates().getCAD());
-                    rates.add("CHF : " + response.body().getRates().getCHF());
-                    rates.add("CNY : " + response.body().getRates().getCNY());
-                    rates.add("CZK : " + response.body().getRates().getCZK());
-                    rates.add("DKK : " + response.body().getRates().getDKK());
-                    rates.add("HRK : " + response.body().getRates().getHRK());
-                    rates.add("GBP : " + response.body().getRates().getGBP());
-                    rates.add("MXN : " + response.body().getRates().getMXN());
-                    rates.add("HKD : " + response.body().getRates().getHKD());
-                    rates.add("HUF : " + response.body().getRates().getHUF());
-                    rates.add("IDR : " + response.body().getRates().getIDR());
-                    rates.add("ILS : " + response.body().getRates().getILS());
-                    rates.add("INR : " + response.body().getRates().getINR());
-                    rates.add("PHP : " + response.body().getRates().getPHP());
-                    rates.add("JPY : " + response.body().getRates().getJPY());
-                    rates.add("MYR : " + response.body().getRates().getMYR());
-                    rates.add("KRW : " + response.body().getRates().getKRW());
-                    rates.add("NOK : " + response.body().getRates().getNOK());
-                    rates.add("NZD : " + response.body().getRates().getNZD());
-                    rates.add("PLN : " + response.body().getRates().getPLN());
-                    rates.add("RON : " + response.body().getRates().getRON());
-                    rates.add("SEK : " + response.body().getRates().getSEK());
-                    rates.add("SGD : " + response.body().getRates().getSGD());
-                    rates.add("USD : " + response.body().getRates().getUSD());
-                    rates.add("ZAR : " + response.body().getRates().getZAR());
-                    rates.add("RUB : " + response.body().getRates().getRUB());
-                    rates.add("TRL : " + response.body().getRates().getTRY());
-                    rates.add("THB : " + response.body().getRates().getTHB());
-                    rates.add("TRY : " + response.body().getRates().getTRY());
-                    rates.remove(defaultCurrency+" : " + response.body().getRates().getClass().getMethod("get"+defaultCurrency).invoke(response.body().getRates()));
-                    exchangeRateArrayAdapter.clear();
-                    exchangeRateArrayAdapter.addAll(rates);
-                    exchangeRateArrayAdapter.notifyDataSetChanged();
+                    updateDisplay(LatestRatesResponse);
                 } catch (Exception e) {
                     Log.d("onResponse", "There is an error");
                     e.printStackTrace();
@@ -131,6 +114,86 @@ public class MainActivity extends AppCompatActivity {
         Spinner spinner = (Spinner) findViewById(spinnerItemId);
         spinner.setAdapter(arrayAdapter);
         return spinner;
+    }
+
+    protected void updateDisplay(Response<LatestExchangeRates> response){
+        String stringValueToConvert =  valueToConvert.getText().toString();
+        if(stringValueToConvert.isEmpty()) {
+            stringValueToConvert = "1";
+            valueToConvert.setText("1");
+        BigDecimal decimalValueToConvert = new BigDecimal(stringValueToConvert);
+
+        List<String> rates = new ArrayList<>();
+
+        if (response.body().getRates().getEUR() != null)
+            rates.add("EUR : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getEUR())));
+        if (response.body().getRates().getAUD() != null)
+            rates.add("AUD : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getAUD())));
+        if (response.body().getRates().getBGN() != null)
+            rates.add("BGN : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getBGN())));
+        if (response.body().getRates().getBRL() != null)
+            rates.add("BRL : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getBRL())));
+        if (response.body().getRates().getCAD() != null)
+            rates.add("CAD : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getCAD())));
+        if (response.body().getRates().getCHF() != null)
+            rates.add("CHF : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getCHF())));
+        if (response.body().getRates().getCNY() != null)
+            rates.add("CNY : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getCNY())));
+        if (response.body().getRates().getCZK() != null)
+            rates.add("CZK : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getCZK())));
+        if (response.body().getRates().getDKK() != null)
+            rates.add("DKK : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getDKK())));
+        if (response.body().getRates().getHRK() != null)
+            rates.add("HRK : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getHRK())));
+        if (response.body().getRates().getGBP() != null)
+            rates.add("GBP : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getGBP())));
+        if (response.body().getRates().getMXN() != null)
+            rates.add("MXN : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getMXN())));
+        if (response.body().getRates().getHKD() != null)
+            rates.add("HKD : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getHKD())));
+        if (response.body().getRates().getHUF() != null)
+            rates.add("HUF : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getHUF())));
+        if (response.body().getRates().getIDR() != null)
+            rates.add("IDR : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getIDR())));
+        if (response.body().getRates().getILS() != null)
+            rates.add("ILS : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getILS())));
+        if (response.body().getRates().getINR() != null)
+            rates.add("INR : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getINR())));
+        if (response.body().getRates().getPHP() != null)
+            rates.add("PHP : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getPHP())));
+        if (response.body().getRates().getJPY() != null)
+            rates.add("JPY : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getJPY())));
+        if (response.body().getRates().getMYR() != null)
+            rates.add("MYR : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getMYR())));
+        if (response.body().getRates().getKRW() != null)
+            rates.add("KRW : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getKRW())));
+        if (response.body().getRates().getNOK() != null)
+            rates.add("NOK : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getNOK())));
+        if (response.body().getRates().getNZD() != null)
+            rates.add("NZD : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getNZD())));
+        if (response.body().getRates().getPLN() != null)
+            rates.add("PLN : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getPLN())));
+        if (response.body().getRates().getRON() != null)
+            rates.add("RON : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getRON())));
+        if (response.body().getRates().getSEK() != null)
+            rates.add("SEK : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getSEK())));
+        if (response.body().getRates().getSGD() != null)
+            rates.add("SGD : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getSGD())));
+        if (response.body().getRates().getUSD() != null)
+            rates.add("USD : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getUSD())));
+        if (response.body().getRates().getZAR() != null)
+            rates.add("ZAR : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getZAR())));
+        if (response.body().getRates().getRUB() != null)
+            rates.add("RUB : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getRUB())));
+        if (response.body().getRates().getTRY() != null)
+            rates.add("TRY : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getTRY())));
+        if (response.body().getRates().getTHB() != null)
+            rates.add("THB : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getTHB())));
+        if (response.body().getRates().getTRY() != null)
+            rates.add("TRY : " + decimalValueToConvert.multiply(new BigDecimal(response.body().getRates().getTRY())));
+        exchangeRateArrayAdapter.clear();
+        exchangeRateArrayAdapter.addAll(rates);
+        exchangeRateArrayAdapter.notifyDataSetChanged();
     }
 
 }
